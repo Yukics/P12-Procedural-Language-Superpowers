f909e45f1870343efd2cae9c3f36be245c3b7473
Alejo <Morell>
2021-05-24 17:25:35 +0200

    6 added

---
+++ Queries/6.sql
@@ -0,0 +1,28 @@
+/* CHECK DATA TYPE
+SELECT DATA_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE
+     TABLE_NAME = 'customers' AND
+     COLUMN_NAME = 'customerid'
+*/
+
+---SQL---
+CREATE FUNCTION show_cust_sql(int) RETURNS customers AS $$
+    SELECT * FROM customers WHERE customerid = $1;
+$$ LANGUAGE SQL;
+
+select * from show_cust_sql(2);
+select (show_cust_sql(2)).firstname;
+
+
+---PLSQL---
+create or replace function show_cust_plpgsql(id INTEGER) returns customers as $$
+declare customer record;
+begin
+    SELECT  * into customer FROM customers WHERE customerid = $1;
+    return customer;
+end; $$ language 'plpgsql';
+
+
+select * from show_cust_plpgsql(2);
+select (show_cust_plpgsql(2)).firstname;
