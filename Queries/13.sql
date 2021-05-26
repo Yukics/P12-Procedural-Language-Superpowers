6d71d37fa49dab6c871555cee3480b562dae265e
Yukics <alejomorell@gmail.com>
2021-05-26 23:13:32 +0200

    P12 Finish and Datagrip conf files

--- Queries/13.sql
+++ Queries/13.sql
@@ -1,17 +1,6 @@
-create or replace function mySum(uno type, dos type) returns numeric as $$
-begin
+create or replace function mySum_sql(uno text, dos text) returns text as $$
+    select(uno::numeric+dos::numeric);
+    select(concat(uno, dos))
+ $$ LANGUAGE sql;
 
-end; $$ language plpgsql;
-
-
-CREATE OR REPLACE FUNCTION foo (VARIADIC arr int[]) RETURNS int AS $$
- DECLARE
-     mySum integer := 0;
-     currVal integer := 0;
- BEGIN
-     FOREACH currVal IN ARRAY arr LOOP
-         mySum := mySum + currVal;
-     END LOOP;
-     RETURN mySum;
- END;
- $$ LANGUAGE plpgsql;
+select mySum_sql(1,1), mySum_sql(1.1, 1), mySum_sql('1','1');
