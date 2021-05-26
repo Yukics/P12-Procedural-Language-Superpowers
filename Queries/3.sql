26f08e8b0ed04928685372e1c4ac8018ff58e150
Yukics <alejomorell@gmail.com>
2021-05-26 23:31:02 +0200

    PLPGSQL & SQL functions

--- Queries/3.sql
+++ Queries/3.sql
@@ -1,33 +1,61 @@
 set search_path = "data";
 
 ---1---
-CREATE function insert_category1_sql (pcategory integer, pname varchar)
+---SQL---
+CREATE FUNCTION insert_category1_sql(pcategory integer, pname varchar) RETURNS void AS
+$$
+    insert into categories (category, categoryname) values (pcategory, pname)
+$$ language sql;
+
+---PLPGSQL---
+CREATE function insert_category1_plpgsql (pcategory integer, pname varchar)
 returns void as $$
 begin
     insert into categories (category, categoryname) values (pcategory, pname);
 end; $$ language plpgsql;
-select insert_category1_sql(21, 'category21');
+select insert_category1_plpgsql(21, 'category21');
 
 ---2---
-CREATE function insert_category2_sql (integer, varchar)
+---SQL---
+CREATE FUNCTION insert_category2_sql(integer, varchar) RETURNS void AS
+$$
+    insert into categories (category, categoryname) values ($1, $2)
+$$ language sql;
+
+---PLPGSQL---
+CREATE function insert_category2_plpgsql (integer, varchar)
 returns void as $$
 begin
     insert into categories (category, categoryname) values ($1, $2);
 end; $$ language plpgsql;
-select insert_category2_sql(22, 'category22');
+select insert_category2_plpgsql(22, 'category22');
 
 ---3---
-CREATE function insert_category3_sql (category integer, categoryname varchar)
+---SQL---
+CREATE FUNCTION insert_category3_sql(category integer, categoryname varchar) RETURNS void AS
+$$
+    insert into categories (category, categoryname) values (category, categoryname)
+$$ language sql;
+
+---PLPGSQL---
+CREATE function insert_category3_plpgsql (category integer, categoryname varchar)
 returns void as $$
 begin
     insert into categories (category, categoryname) values (category, categoryname);
 end; $$ language plpgsql;
-select insert_category3_sql(23, 'category23');
+select insert_category3_plpgsql(23, 'category23');
 
 ---4---
-CREATE function insert_category4_sql (pcategory categories)
+---SQL---
+CREATE FUNCTION insert_category4_sql(pcategory categories) RETURNS void AS
+$$
+    insert into categories values ($1.category, $1.categoryname)
+$$ language sql;
+
+---PLPGSQL---
+CREATE function insert_category4_plpgsql (pcategory categories)
 returns void as $$
 begin
-    insert into categories values (pcategory);
+    insert into categories values ($1.category, $1.categoryname);
 end; $$ language plpgsql;
-select insert_category4_sql((24, 'category24'));
+select insert_category4_plpgsql((24, 'category24'));
